e05171b30b4ad09ff8eaa014c3e1ee0911f2c833
Fagner Viana <fagner.viana@domvsit.com.br>
2022-07-11 19:07:58 -0300

    feature-5: implement and integrated react route-dom

--- src/components/Promocao/Cartao/Cartao.js
+++ src/components/Promocao/Cartao/Cartao.js
@@ -4,7 +4,7 @@ import './Cartao.scss';
 const PromocaoCartao = ({ ...props }) => {
     return (
         <div className='promocao-cartao'>
-            <img src={props.promocao.imageUrl} alt='imagem' className='promocao-cartao__imagem' />
+            <img src={props.promocao.imageUrl} alt={props.promocao.title} className='promocao-cartao__imagem' />
             <div className='promocao-cartao__info'>
                 <h1 className='promocao-cartao__title'>{props.promocao.title}</h1>
                 <span className='promocao-cartao__price'>R$ {props.promocao.price}</span>
@@ -12,10 +12,10 @@ const PromocaoCartao = ({ ...props }) => {
                     {props.promocao.comments.length > 0 && (
                         <div className='promocao-cartao__comentario'>{props.promocao.comments[0].comment}</div>
                     )}
-                    <div className='promocao-cartao__comentario-count'>{props.promocao.comments.length}{' '} 
+                    <div className='promocao-cartao__comentario-count'>{props.promocao.comments.length}{' '}
                         {props.promocao.comments.length > 1 ? 'Comentários' : ' Comentário '}
                     </div>
-                    <a href={props.promocao.url} target='blank' className='promocao-cartao__link'>IR PARA O SITE</a>
+                    <a href={props.promocao.url} target='blank' className='promocao-cartao__link' >IR PARA O SITE</a>
                 </footer>
             </div>
         </div>
